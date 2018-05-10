7e1b979b349ba9492ef017e1b31c17cf2c9351f1
annabelletrinh <trinhannabelle@gmail.com>
2018-05-10 13:38:13 +0800

    Initialization

---
+++ BakingConverter/src/identity/Controller.java
@@ -0,0 +1,326 @@
+package identity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.math.RoundingMode;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.text.DecimalFormat;
+import java.util.LinkedHashMap;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+	private static final String RESOURCE_PATH = "/identity/resources/";
+    private LinkedHashMap<String, Double> masseVolumique;
+    private LinkedHashMap<String, Double> litre;
+    private LinkedHashMap<String, Double> gramme;
+    private LinkedHashMap<String, Double> volume;
+    private LinkedHashMap<String, Double> masse;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    TextField input;
+
+    @FXML
+    private TextField output;
+
+    @FXML
+    TextField cel;
+
+    @FXML
+    TextField inMasse;
+
+    @FXML
+    TextField inVol;
+
+    @FXML
+    TextField far;
+
+    @FXML
+    private TextField outMasse;
+
+    @FXML
+    private TextField outVol;
+
+    @FXML
+    ChoiceBox<String> inputUnit;
+
+    @FXML
+    ChoiceBox<String> outputUnit;
+    
+    @FXML
+	ChoiceBox<String> unitInMasse;
+
+    @FXML
+    ChoiceBox<String> unitInVol;
+    
+    @FXML
+    ChoiceBox<String> unitOutVol;
+
+    @FXML
+    ChoiceBox<String> unitOutMasse;
+    
+    @FXML
+    ChoiceBox<String> items;
+ 
+    void cel2Far(String newValue){
+		double celcius = 0;
+    	try{
+    		if(StringUtils.isNotBlank(newValue)){
+    			celcius = Double.parseDouble(newValue.trim().replace(",","."));
+    		}
+    		double value = celcius*1.8 + 32;
+    		DecimalFormat df = new DecimalFormat("#.#");
+    		df.setRoundingMode(RoundingMode.CEILING);
+        	far.setText(df.format(value));
+    	}catch(NumberFormatException e){
+    		far.setText("NULL");
+    	}
+    }
+
+
+    void far2Cel(String newValue) {
+    	
+    	double fare = 0.0;
+       	try{
+    		if(StringUtils.isNotBlank(newValue)){
+        		fare = Double.parseDouble(newValue.trim().replace(",","."));
+        	}
+           	double value = ( fare - 32 )/1.8;
+    		DecimalFormat df = new DecimalFormat("#.#");
+    		df.setRoundingMode(RoundingMode.CEILING);
+        	cel.setText(df.format(value));
+       	}catch(NumberFormatException e){
+       		cel.setText("NULL");
+       	}
+
+    	
+    }
+
+    void convMass(String newValue) {
+   		double qt = 0.0;
+   		try{
+    		if(StringUtils.isNotBlank(newValue)){
+       			qt = Double.parseDouble(newValue.trim().replace(",","."));
+        	}
+       		double output = qt*(masse.get(unitInMasse.getValue())/masse.get(unitOutMasse.getValue()));
+    		DecimalFormat df = new DecimalFormat("#.###");
+    		df.setRoundingMode(RoundingMode.CEILING);
+       		outMasse.setText(df.format(output));
+   		}catch(NumberFormatException e){
+   			outMasse.setText("NULL");
+   		}
+    }
+   
+    void convVol(String newValue){
+		double qt = 0.0;
+		try{
+	  		if(StringUtils.isNotBlank(newValue)){
+       			qt = Double.parseDouble(newValue.trim().replace(",","."));
+        	}    		
+	  		double output = qt*(volume.get(unitInVol.getValue())/volume.get(unitOutVol.getValue()));
+    		DecimalFormat df = new DecimalFormat("#.###");
+    		df.setRoundingMode(RoundingMode.CEILING);
+        	outVol.setText(df.format(output));
+		}catch(NumberFormatException e){
+			outVol.setText("NULL");
+		}
+    }
+    
+    void convMetricSystem(String newValue, boolean isInput, String dimension) {
+		double qt = 0.0;
+		double unitInput = 0.0;
+		double unitOutput = 0.0;
+		switch(dimension.toLowerCase()) {
+		case "volume":
+			if(isInput){
+				unitInput = volume.get(newValue);
+				unitOutput = volume.get(unitOutVol.getValue());
+			}else{
+				unitInput = volume.get(unitInVol.getValue());
+				unitOutput = volume.get(newValue);
+			}
+			try{
+				qt = Double.parseDouble(inVol.getText().trim().replace(",","."));
+	    		double output = qt*(unitInput/unitOutput);
+	    		outVol.setText(Double.toString(output));
+			}catch(NumberFormatException e){
+				outVol.setText("NULL");
+			}   
+			break;
+		case "masse":
+			if(isInput){
+				unitInput = masse.get(newValue);
+				unitOutput = masse.get(unitOutMasse.getValue());
+			}else{
+				unitInput = masse.get(unitInMasse.getValue());
+				unitOutput = masse.get(newValue);
+			}
+    		try{
+
+    			qt = Double.parseDouble(inMasse.getText().trim().replace(",","."));
+        		double output = qt*(unitInput/unitOutput);
+        		outMasse.setText(Double.toString(output));
+    		}catch(NumberFormatException e){
+    			outMasse.setText("NULL");
+    		}    	
+			break;
+		default:
+			break;
+		}
+		
+	
+    }
+
+
+    
+    
+    void convertVol2Weight(String newValue, boolean isInput, String ingOrUnit){
+    	String iUnit = "";
+    	String oUnit = "";
+       	String pickedItem = newValue;
+       	
+    	switch (ingOrUnit.toLowerCase()){
+    	case "ingredient":
+    		pickedItem = newValue;
+    		oUnit = outputUnit.getValue();
+    		iUnit = inputUnit.getValue();
+    		break;
+    	case "unit":
+        	if(isInput){
+        		iUnit = newValue;
+        		oUnit = outputUnit.getValue();
+        	}else{
+        		iUnit = inputUnit.getValue();
+        		oUnit = newValue;
+        	}
+           	pickedItem = items.getValue();
+           	break;
+        default:
+    		oUnit = outputUnit.getValue();
+    		iUnit = inputUnit.getValue();
+           	pickedItem = items.getValue();
+        	break;
+    	}
+
+       	double value= 0.0;
+       	double inQt = 0.0;
+       	try{
+	  		if(StringUtils.isNotBlank(input.getText())){
+       			inQt = Double.parseDouble(input.getText().trim().replace(",","."));
+        	}  
+           	if(iUnit.contains("L") && oUnit.contains("L") ){
+           		value = inQt * litre.get(iUnit) / litre.get(oUnit);
+           	}else if (iUnit.contains("g") && oUnit.contains("g")){
+           		value = inQt * gramme.get(iUnit) / gramme.get(oUnit);
+            }else if (iUnit.contains("g")){
+           		value = inQt * gramme.get(iUnit) * litre.get(oUnit )/ masseVolumique.get(pickedItem);
+           	}else{
+           		value = inQt * litre.get(iUnit) * masseVolumique.get(pickedItem) / gramme.get(oUnit);
+           	}
+       		output.setText(Double.toString(value));
+        	}catch(NumberFormatException e){
+        		output.setText("NULL");
+        	}
+    }
+   
+    void convertVol2Weight(String newValue){
+		String oUnit = outputUnit.getValue();
+		String iUnit = inputUnit.getValue();
+       	String pickedItem = items.getValue();
+       	double value= 0.0;
+       	double inQt = 0.0;
+       	try{
+	  		if(StringUtils.isNotBlank(newValue)){
+       			inQt = Double.parseDouble(newValue.trim().replace(",","."));
+        	}             	if(iUnit.contains("L") && oUnit.contains("L") ){
+           		value = inQt * litre.get(iUnit) / litre.get(oUnit);
+           	}else if (iUnit.contains("g") && oUnit.contains("g")){
+           		value = inQt * gramme.get(iUnit) / gramme.get(oUnit);
+            }else if (iUnit.contains("g")){
+           		value = inQt * gramme.get(iUnit) * litre.get(oUnit )/ masseVolumique.get(pickedItem);
+           	}else{
+           		value = inQt * litre.get(iUnit) * masseVolumique.get(pickedItem) / gramme.get(oUnit);
+           	}
+       		output.setText(Double.toString(value));
+        	}catch(NumberFormatException e){
+        		output.setText("NULL");
+        	}
+    }
+
+    private void loadVol2WeightData() throws IOException, URISyntaxException{
+		masseVolumique = getMapFromCSV("massevolumique.txt");
+		items.getItems().addAll(masseVolumique.keySet().stream().sorted().collect(Collectors.toList()));
+    	items.getSelectionModel().selectFirst();
+
+		litre = getMapFromCSV("litre.txt");
+		gramme = getMapFromCSV("gramme.txt");
+    	inputUnit.getItems().addAll(litre.keySet());
+    	inputUnit.getItems().addAll(gramme.keySet());
+    	outputUnit.getItems().addAll(litre.keySet());
+    	outputUnit.getItems().addAll(gramme.keySet());
+    	inputUnit.getSelectionModel().selectFirst();
+    	outputUnit.getSelectionModel().select(3);
+    
+    }
+    
+    private void loadMetConvData() throws IOException, URISyntaxException{
+		volume = getMapFromCSV("volume.txt");
+		masse = getMapFromCSV("masse.txt");
+		unitInMasse.getItems().addAll(masse.keySet());
+		unitOutMasse.getItems().addAll(masse.keySet());
+		unitInVol.getItems().addAll(volume.keySet());
+		unitOutVol.getItems().addAll(volume.keySet());
+		unitInMasse.getSelectionModel().selectFirst();
+		unitInVol.getSelectionModel().selectFirst();
+		unitOutMasse.getSelectionModel().selectFirst();
+		unitOutVol.getSelectionModel().selectFirst();
+
+    }
+    
+    @FXML
+    void initialize() throws IOException, URISyntaxException {
+        assert items != null : "fx:id=\"item\" was not injected: check your FXML file 'MainView.fxml'.";
+        assert input != null : "fx:id=\"input\" was not injected: check your FXML file 'MainView.fxml'.";
+        assert output != null : "fx:id=\"output\" was not injected: check your FXML file 'MainView.fxml'.";
+        assert inputUnit != null : "fx:id=\"inputUnit\" was not injected: check your FXML file 'MainView.fxml'.";
+        assert outputUnit != null : "fx:id=\"outputUnit\" was not injected: check your FXML file 'MainView.fxml'.";
+    	loadVol2WeightData();
+    	loadMetConvData();
+
+
+    }
+    
+	public static LinkedHashMap<String, Double> getMapFromCSV(final String filePath) throws IOException, URISyntaxException{
+
+    	InputStream in = Controller.class.getResourceAsStream(RESOURCE_PATH+filePath);
+    	
+    	LinkedHashMap<String, Double> resultMap = new LinkedHashMap<String,Double>();
+    	
+    	BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+    	String line;
+    	while( (line = reader.readLine()) != null ){
+    		resultMap.put(line.split(",")[0], Double.parseDouble(line.split(",")[1]));
+    	}
+    	reader.close();
+    	
+        return resultMap;
+    }
+    
+
+}
---
+++ BakingConverter/src/identity/Main.java
@@ -0,0 +1,117 @@
+package identity;
+
+import java.io.IOException;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+import org.apache.commons.lang3.StringUtils;
+
+import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	private static final String RESOURCE_PATH = "/identity/resources/";
+	private static final String VIEW_PATH = "/identity/view/";
+
+	private Stage primaryStage;
+	private BorderPane mainLayout;
+	
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Baking Converter");
+		this.primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream(RESOURCE_PATH+"muffin.png")));
+		showMainView();
+		
+	}
+	
+	private void showMainView() throws IOException{
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource(VIEW_PATH+"MainView.fxml"));
+		mainLayout = loader.load();
+		Scene scene = new Scene(mainLayout);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		Controller controller = loader.getController();
+		controller.unitInMasse.getSelectionModel().selectedItemProperty().addListener((v,oldValue,newValue) -> controller.convMetricSystem(newValue,true,"masse"));;
+		controller.unitOutMasse.getSelectionModel().selectedItemProperty().addListener((v,oldValue,newValue) -> controller.convMetricSystem(newValue,false,"masse"));;
+		controller.unitInVol.getSelectionModel().selectedItemProperty().addListener((v,oldValue,newValue) -> controller.convMetricSystem(newValue,true,"volume"));;
+		controller.unitOutVol.getSelectionModel().selectedItemProperty().addListener((v,oldValue,newValue) -> controller.convMetricSystem(newValue,false,"volume"));;
+		controller.items.getSelectionModel().selectedItemProperty().addListener((v,oldValue,newValue) -> controller.convertVol2Weight(newValue, false, "ingredient"));
+		controller.inputUnit.getSelectionModel().selectedItemProperty().addListener((v,oldValue,newValue) -> controller.convertVol2Weight(newValue, true, "unit"));
+		controller.outputUnit.getSelectionModel().selectedItemProperty().addListener((v,oldValue,newValue) -> controller.convertVol2Weight(newValue, false, "unit"));
+		controller.input.textProperty().addListener((v,oldValue,newValue) -> controller.convertVol2Weight(newValue));
+		controller.inVol.textProperty().addListener((v,oldValue,newValue) -> controller.convVol(newValue));
+		controller.inMasse.textProperty().addListener((v,oldValue,newValue) -> controller.convMass(newValue));
+		final StringProperty textCel = new SimpleStringProperty(controller.cel.getText());
+		textCel.addListener(new ChangeListener<String>() {
+		    @Override
+		    public void changed(ObservableValue<? extends String> obs, String oldValue, String newValue) {
+		        controller.cel.setText(newValue);
+		    }
+		});
+		
+		controller.cel.textProperty().addListener(new ChangeListener<String>() {
+		    @Override
+		    public void changed(ObservableValue<? extends String> obs, String oldValue, String newValue) {
+		        if (! newValue.equals(textCel.get())) { 
+		    		double celcius = 0;
+		        	try{
+		        		if(StringUtils.isNotBlank(newValue)){
+		        			celcius = Double.parseDouble(newValue.trim().replace(",","."));
+		        		}
+		        		double value = celcius*1.8 + 32;
+		        		DecimalFormat df = new DecimalFormat("#.#");
+		        		df.setRoundingMode(RoundingMode.CEILING);
+		        		controller.far.setText(df.format(value));
+		        	}catch(NumberFormatException e){
+		        		controller.far.setText("NULL");
+		        	}   
+		        	textCel.set(newValue);
+		        }
+		    }
+		});
+		final StringProperty textFar = new SimpleStringProperty(controller.far.getText());
+		textFar.addListener(new ChangeListener<String>() {
+		    @Override
+		    public void changed(ObservableValue<? extends String> obs, String oldValue, String newValue) {
+		        controller.far.setText(newValue);
+		    }
+		});
+		
+		controller.far.textProperty().addListener(new ChangeListener<String>() {
+		    @Override
+		    public void changed(ObservableValue<? extends String> obs, String oldValue, String newValue) {
+		        if (! newValue.equals(textFar.get())) { 
+		        	double fare = 0.0;
+		           	try{
+		        		if(StringUtils.isNotBlank(newValue)){
+		            		fare = Double.parseDouble(newValue.trim().replace(",","."));
+		            	}
+		               	double value = ( fare - 32 )/1.8;
+		        		DecimalFormat df = new DecimalFormat("#.#");
+		        		df.setRoundingMode(RoundingMode.CEILING);
+		            	controller.cel.setText(df.format(value));
+		           	}catch(NumberFormatException e){
+		           		controller.cel.setText("NULL");
+		           	}
+		        	textFar.set(newValue);
+		        }
+		    }
+		});
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
